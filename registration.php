1b345006373c01f90ab58e7ab63507b6b157714a
manjilatul jannat <manjilatulsetu@gmail.com>
2021-03-15 12:12:04 +0600

    Initial commit

---
+++ registration.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> REGISTRATION </title>
+</head>
+<body>
+<h2> REGISTRATION </h2>
+
+<br> 
+
+<form>
+<table>
+
+<tr> 
+<td> Name </td>
+<td> <input type = "text" id = "name" name = "name" ></td>
+ </tr>
+
+ <tr>
+ <td> Email: </td>
+ <td> <input type = "text" id = "email" name = "email"> </td>
+ </tr>
+
+ <tr> 
+ <td> User Name:</td>
+ <td><input type = "text" id = "lname" name = "lname" ></td>
+ </tr>
+
+ <tr>
+ <td>  Password: </td> 
+ <td> <input type = "password" id = "ename" name = "ename"></td>
+ </tr>
+
+ <tr>
+ <td> Confirm Password: </td> 
+ <td> <input type = "password" id = "password" name = "password"></td>
+ </tr>
+ </table> 
+
+<h1> Gender  </h1>
+<input type = "radio" id = "male" name = "gender" value ="male" >
+Male 
+<input type = "radio" id = "female" name = "gender" value ="female" >
+Female 
+<input type = "radio" id = "other" name = "gender" value ="other" >
+others
+
+
+<h1> Date Of Birth </h1>
+<input type = "date" id = "birthday" name = "birthday" value ="birthday" >
+<br> <br>
+
+ <input type = "submit" value = "SUBMIT">
+ <input type = "reset" value = "RESET">
+ 
+
+</form>
+</body> 
+</html>
